b2ab62e593947176534f0152c7986aed7452d88a
Ruslan Negoda <103960102+RuslanNegoda1989Arjuna@users.noreply.github.com>
2024-01-17 18:41:30 +0200

    finish

--- src/components/CalculatorTo10/CalculatorTo10.styled.js
+++ src/components/CalculatorTo10/CalculatorTo10.styled.js
@@ -1,7 +1,7 @@
 // Calculate.styled.js
 import styled from '@emotion/styled';
 
-export const BUTTON_SIZE = 30;
+export const BUTTON_SIZE = 35;
 
 const CalculatorContainer = styled.div`
   display: flex;
@@ -19,7 +19,7 @@ const StairButton = styled.button`
   width: ${BUTTON_SIZE}px;
   height: ${BUTTON_SIZE}px;
   margin: 0;
-  font-size: 12px;
+  font-size: 16px;
   cursor: pointer;
   border: 1px solid #ccc;
   background-color: ${(props) => (props.active ? '#fdbb2d' : 'rgb(250, 240, 40, 0.3)')};
@@ -30,43 +30,58 @@ const StairButton = styled.button`
   margin-bottom: 0px;
   cursor: pointer;
 
-border: none;
+ border: none;
  color: rgb(45, 45, 45);
  border-radius: 5px;
- box-shadow: '0px 0.7px 3px rgba(0, 0, 0, 0.32)',;
- /* background-color: rgb(250, 240, 40, 0.3);
- :hover {
-   background-color: #fdbb2d;
- }
-:active {
-  transform: translateY(1px);
-} */
+ box-shadow: '0px 0.7px 3px rgba(0, 0, 0, 0.32)';
+
 `;
 
 const OperationButtonContainer = styled.div`
   display: flex;
-  flex-direction: row;
+  justify-content: center;
+  /* flex-direction: row; */
   margin-top: 10px;
 `;
 
-const OperationButton = styled(StairButton)`
-  width: ${BUTTON_SIZE * 2}px;
+const OperationButton = styled.button`
+ width: ${BUTTON_SIZE * 3}px;
+  height: ${BUTTON_SIZE * 3}px;
+  padding: 10px;
+  /* margin: 0 auto; */
+  margin-right: 20px;
+  font-weight: 700;
+  text-transform: uppercase;
+  cursor: pointer;
+
+  border: none;
+  color: rgb(45, 45, 45);
+  border-radius: 5px;
+  box-shadow: '0px 0.7px 3px rgba(0, 0, 0, 0.32)';
+  background-color: rgb(250, 240, 40, 0.3);
+  :hover {
+    background-color: #fdbb2d;
+  }
+  :active {
+    transform: translateY(1px);
+  }
+  /* width: ${BUTTON_SIZE * 3}px;
+  height: ${BUTTON_SIZE * 3}px;
   margin: 5px;
   margin-bottom: 0;
+  cursor: pointer;
+  border: 1px solid #ccc;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0;
+  margin-bottom: 0px;
+  cursor: pointer;
+  border: none;
+  color: rgb(45, 45, 45);
+  border-radius: 5px;
+  box-shadow: '0px 0.7px 3px rgba(0, 0, 0, 0.32)';
+  background-color: ${(props) => (props.active ? '#fdbb2d' : 'rgb(250, 240, 40, 0.3)')}; */
 `;
-
-// cursor: pointer;
-
-//   border: none;
-//   color: rgb(45, 45, 45);
-//   border-radius: 5px;
-//   box-shadow: shadow: '0px 0.7px 3px rgba(0, 0, 0, 0.32)',;
-//   background-color: rgb(250, 240, 40, 0.3);
-//   :hover {
-//     background-color: #fdbb2d;
-//   }
-//   :active {
-//     transform: translateY(1px);
-//   }
 
 export { CalculatorContainer, StaircaseContainer, StairButton, OperationButtonContainer, OperationButton };
